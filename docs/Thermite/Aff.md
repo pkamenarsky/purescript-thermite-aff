c07bf2533231e1f6a20620526233611796a8403d
Phil Freeman <paf31@cantab.net>
2016-01-24 12:20:09 -0800

    Update to thermite 0.13.0

--- docs/Thermite/Aff.md
+++ docs/Thermite/Aff.md
@@ -6,25 +6,25 @@ and the `purescript-aff` asynchronous effect monad.
 #### `asyncOne`
 
 ``` purescript
-asyncOne :: forall eff state props action. (action -> props -> state -> Aff eff state) -> (Error -> state) -> PerformAction eff state props action
+asyncOne :: forall eff state props action. (action -> props -> state -> Aff eff (state -> state)) -> (Error -> state -> state) -> PerformAction eff state props action
 ```
 
-Create a `PerformAction` handler from a function which returns a single new state asynchronously.
+Create a `PerformAction` handler from a function which returns a single new state update asynchronously.
 
 #### `asyncOne'`
 
 ``` purescript
-asyncOne' :: forall eff state props action. (action -> props -> state -> Aff eff state) -> PerformAction eff state props action
+asyncOne' :: forall eff state props action. (action -> props -> state -> Aff eff (state -> state)) -> PerformAction eff state props action
 ```
 
-Create a `PerformAction` handler from a function which returns a single new state asynchronously.
+Create a `PerformAction` handler from a function which returns a single new state update asynchronously.
 
 On error, this function writes its error to the console.
 
 #### `asyncMany`
 
 ``` purescript
-asyncMany :: forall eff state props action. (action -> props -> state -> Producer state (Aff eff) Unit) -> PerformAction eff state props action
+asyncMany :: forall eff state props action. (action -> props -> state -> Producer (state -> state) (Aff eff) Unit) -> PerformAction eff state props action
 ```
 
 Create a `PerformAction` handler from a function which returns many new states asynchronously.
--- src/Thermite/Aff.purs
+++ src/Thermite/Aff.purs
@@ -22,18 +22,18 @@ import qualified Control.Coroutine as C
 
 import qualified Thermite as T
 
--- | Create a `PerformAction` handler from a function which returns a single new state asynchronously.
+-- | Create a `PerformAction` handler from a function which returns a single new state update asynchronously.
 asyncOne :: forall eff state props action. 
-            (action -> props -> state -> Aff eff state) ->
-            (Error -> state) ->
+            (action -> props -> state -> Aff eff (state -> state)) ->
+            (Error -> state -> state) ->
             T.PerformAction eff state props action
 asyncOne f fromError action props state k = runAff (k <<< fromError) k (f action props state)
 
--- | Create a `PerformAction` handler from a function which returns a single new state asynchronously.
+-- | Create a `PerformAction` handler from a function which returns a single new state update asynchronously.
 -- |
 -- | On error, this function writes its error to the console.
 asyncOne' :: forall eff state props action. 
-             (action -> props -> state -> Aff eff state) ->
+             (action -> props -> state -> Aff eff (state -> state)) ->
              T.PerformAction eff state props action
 asyncOne' f action props state k = runAff unsafeLog k (f action props state)
   where
@@ -45,12 +45,12 @@ asyncOne' f action props state k = runAff unsafeLog k (f action props state)
 -- | All errors are ignored so users should use `attempt` or some other means of catching asynchronous
 -- | exceptions.
 asyncMany :: forall eff state props action. 
-             (action -> props -> state -> C.Producer state (Aff eff) Unit) ->
+             (action -> props -> state -> C.Producer (state -> state) (Aff eff) Unit) ->
              T.PerformAction eff state props action
 asyncMany f action props state k = unsafeInterleaveEff $ launchAff $ C.runProcess process
   where
   process :: C.Process (Aff eff) Unit
   process = f action props state $$ hoistFreeT liftEff consumer
 
-  consumer :: C.Consumer state (Eff eff) Unit
+  consumer :: C.Consumer (state -> state) (Eff eff) Unit
   consumer = C.consumer \state -> map Just (k state)
